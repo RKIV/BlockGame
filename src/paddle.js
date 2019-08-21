bdff3eb368f12bd7555f09362bacbab6fda96329
Robert Keller <robertkellerIV@gmail.com>
2019-08-21 13:29:20 -0400

    Add input handling and movement

--- src/paddle.js
+++ src/paddle.js
@@ -1,12 +1,25 @@
 export default class Paddle {
     constructor(gameWidth, gameHeight) {
+        this.gameHeight = gameHeight;
+        this.gameWidth = gameWidth;
         this.width = 150;
         this.height = 20;
+
+        this.maxSpeed = .5;
+        this.speed = 0;
         
         this.position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10,
         };
+    }
+
+    moveLeft() {
+        this.speed = -this.maxSpeed;
+    }
+
+    moveRight() {
+        this.speed = this.maxSpeed;
     }
 
     draw(ctx) {
@@ -17,6 +30,9 @@ export default class Paddle {
 
     update(deltaTime) {
         if(!deltaTime) return;
-        this.position.x += 0.1 * deltaTime;
+        this.position.x += this.speed * deltaTime
+
+        if(this.position.x < 0) this.position.x = 0;
+        if(this.position.x > this.gameWidth-this.width) this.position.x = this.gameWidth-this.width;
     }
 }
